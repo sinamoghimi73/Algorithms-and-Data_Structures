5188a076c5f690d19ce4432c3d58478ad4783fa7
sinamoghimi73 <45493197+sinamoghimi73@users.noreply.github.com>
2020-05-11 00:43:28 +0430

    ...

--- Algorithmic_Toolbox/Binary_Search/Binary_Search.cpp
+++ Algorithmic_Toolbox/Binary_Search/Binary_Search.cpp
@@ -2,27 +2,28 @@
 #include <vector>
 
 
-int Binary_Search(const std::vector<int> &vec, int x){
+int Binary_Search(const std::vector<int> &vec, int *x){
     int left{0}, right{(int)(vec.size()-1)}, mid{0};
 
     while (((right - left) / 2) > 0){
         mid = left + ((right - left) / 2);
-        // std::cout << vec.at(mid) << "\n";
-        if (x > vec.at(mid)){
+
+        if (*x > vec.at(mid)){
             left = mid;
             mid = left + ((right - left) / 2);
         }
 
-        if (x < vec.at(mid)){
+        if (*x < vec.at(mid)){
             right = mid;
             mid = left + ((right - left) / 2);
         }
 
-        if (x == vec.at(mid)){
+        if (*x == vec.at(mid)){
             return mid;
         }
     }
-	if(x == vec.at(mid+1)){
+    
+	if(*x == vec.at(mid+1)){
 		return mid+1;
 	}
 	
@@ -41,22 +42,11 @@ int main(){
         vec.push_back(num);
     }
 
-    // std::cout << Binary_Search(vec, 5) << " ";
 
-    int m{0};
-    std::cin >> m;
+    int x{0};
+    std::cin >> x;
 
-//    std::vector<int> num_to_find(0);
-//    // int num{0};
-//    for(int i = 0; i < m; i++){
-//        std::cin >> num;
-//        num_to_find.push_back(num);
-//    }
-//
-//    for(int i = 0; i < m; i++){
-//        std::cout << Binary_Search(vec, num_to_find.at(i)) << " ";
-//    }
-	std::cout << Binary_Search(vec, m) << " ";
+	std::cout << Binary_Search(vec, &x) << "\n";
 
 
     return 0;
