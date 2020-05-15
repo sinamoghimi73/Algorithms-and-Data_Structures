69125a6994644a9ab79c9291ddcc2971f206acff
sinamoghimi73 <45493197+sinamoghimi73@users.noreply.github.com>
2020-05-15 22:30:33 +0430

    ...

--- Algorithmic_Toolbox/APlusB/APlusB.cpp
+++ Algorithmic_Toolbox/APlusB/APlusB.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 
-int Add(int *num1, int *num2){
-    return *num1 + *num2;
-}
+int Add(int *num1, int *num2) { return *num1 + *num2; }
 
-int main(){
+int main() {
 
-    int a{},b{};
+  int a{}, b{};
 
-	std::cin>>a>>b;
+  std::cin >> a >> b;
 
-	std::cout<< Add(&a, &b) <<std::endl;
-	
-	return 0;
+  std::cout << Add(&a, &b) << std::endl;
+
+  return 0;
 }
