5f5979beab3adade2a2f64529855a67ea7399a58
sinamoghimi73 <45493197+sinamoghimi73@users.noreply.github.com>
2020-05-15 22:06:32 +0430

    Update Binary_Search.cpp

--- Algorithmic_Toolbox/Binary_Search/Binary_Search.cpp
+++ Algorithmic_Toolbox/Binary_Search/Binary_Search.cpp
@@ -1,53 +1,49 @@
 #include <iostream>
 #include <vector>
 
+int Binary_Search(const std::vector<int> &vec, int &x) {
+  int left{0}, right{static_cast<int>(vec.size() - 1)}, mid{0};
 
-int Binary_Search(const std::vector<int> &vec, int *x){
-    int left{0}, right{(int)(vec.size()-1)}, mid{0};
+  while (((right - left) / 2) > 0) {
+    mid = left + ((right - left) / 2);
 
-    while (((right - left) / 2) > 0){
-        mid = left + ((right - left) / 2);
-
-        if (*x > vec.at(mid)){
-            left = mid;
-            mid = left + ((right - left) / 2);
-        }
-
-        if (*x < vec.at(mid)){
-            right = mid;
-            mid = left + ((right - left) / 2);
-        }
-
-        if (*x == vec.at(mid)){
-            return mid;
-        }
+    if (x > vec.at(mid)) {
+      left = mid;
+      mid = left + ((right - left) / 2);
     }
-    
-	if(*x == vec.at(mid+1)){
-		return mid+1;
-	}
-	
-    return -1;
+
+    if (x < vec.at(mid)) {
+      right = mid;
+      mid = left + ((right - left) / 2);
+    }
+
+    if (x == vec.at(mid)) {
+      return mid;
+    }
+  }
+
+  if (x == vec.at(mid + 1)) {
+    return mid + 1;
+  }
+
+  return -1;
 }
 
+int main() {
+  int n{0};
+  std::cin >> n;
 
-int main(){
-    int n{0};
-    std::cin >> n;
+  std::vector<int> vec(0);
+  int num{0};
+  for (int i = 0; i < n; i++) {
+    std::cin >> num;
+    vec.push_back(num);
+  }
 
-    std::vector<int> vec(0);
-    int num{0};
-    for(int i = 0; i < n; i++){
-        std::cin >> num;
-        vec.push_back(num);
-    }
+  int x{0};
+  std::cin >> x;
 
+  std::cout << Binary_Search(vec, x) << "\n";
 
-    int x{0};
-    std::cin >> x;
-
-	std::cout << Binary_Search(vec, &x) << "\n";
-
-
-    return 0;
+  return 0;
 }
